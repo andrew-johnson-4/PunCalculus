953354223ee75055a6186332073ea072279986d5
Andrew Johnson <andrew@subarctic.org>
2023-10-07 20:43:01 -0600

    start writing out regression tests

---
+++ tests/individual_rules.rs
@@ -0,0 +1,14 @@
+
+//single term | terminal?
+//x           | False
+//x y         | False
+//\           | False
+//\<x.x>      | False
+//\<x.x><y.y> | False
+//x: ?        | False
+//x: \bot     | True
+//x: \top     | True
+//x: A        | True
+//x: 0 -> A   | True
+//x: 1 -> A   | True
+//x: A -> B   | True
