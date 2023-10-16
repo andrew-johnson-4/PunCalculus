14ac216376c2625aec7771de47dd978aef69e2b2
Andrew Johnson <andrew@subarctic.org>
2023-10-15 20:49:18 -0600

    start testing type inference

---
+++ tests/b_types.rs
@@ -0,0 +1,36 @@
+use ::punc::{punc,Term,Type};
+
+#[test]
+fn structural_equality() {
+
+   assert_eq!(
+      punc!( a: A ).typ(),
+      Type::named("A")
+   );
+   assert_eq!(
+      punc!( b: B ).typ(),
+      Type::named("B")
+   );
+   assert_eq!(
+      punc!( c: C ).typ(),
+      Type::named("C")
+   );
+
+   assert_eq!(
+      punc!( a: A+B+C ).typ(),
+      Type::plural(vec![
+         Type::named("A"),
+         Type::named("B"),
+         Type::named("C")
+      ])
+   );
+
+   assert_eq!(
+      punc!( a: A->B ).typ(),
+      Type::arrow(
+         Type::named("A"),
+         Type::named("B")
+      )
+   );
+
+}
