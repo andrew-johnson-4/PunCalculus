48481afd21b9ecdbc753b5f20092d237050d619e
Andrew Johnson <andrew@subarctic.org>
2023-11-12 22:28:47 -0700

    more simple tests

--- tests/c_lsts.rs
+++ tests/c_lsts.rs
@@ -4,3 +4,12 @@
 
 //narrow application function
 //(f: A -> B + C -> D)(x: A): B
+
+//split application
+//(f: A -> B + A -> C)(x: A): B + C
+
+//merge application
+//(f: A -> C + B -> C)(x: B + C): C
+
+//carry application
+//(F: A -> B + C -> D)(x: A + C): B + D
