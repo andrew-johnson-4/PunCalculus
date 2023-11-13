1bc8efa53a3622c7171d85c1f6a6afa3e4f16c7b
Andrew Johnson <andrew@subarctic.org>
2023-11-12 22:43:48 -0700

    lingo

--- tests/c_lsts.rs
+++ tests/c_lsts.rs
@@ -1,4 +1,10 @@
 
+//plural applications can be categorized into splits, merges, carries, and narrows
+//splits turn singular types into plural types
+//merges turn plural types into singular types
+//carries apply pluralities of functions and values
+//narrows discard some lines of computation entirely
+
 //narrow application argument
 //(f: A -> B)(x: A + C): B
 
