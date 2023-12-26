b7972a97e276ad2a46865e765f1845067335cd18
Andrew Johnson <andrew@subarctic.org>
2023-12-26 15:38:48 -0700

    passive as syntax check

--- src/term.rs
+++ src/term.rs
@@ -11,14 +11,15 @@ pub enum Term {
    App(Box<Term>,Box<Term>,Type),
 }
 
-const BINOP: [&'static str; 2] = ["movl","offset"];
+const BINOP: [&'static str; 3] = ["mov", "movl", "xor"];
 const UNOP: [&'static str; 1] = ["int"];
+const ZOP: [&'static str; 1] = ["syscall"];
 
 impl Term {
    pub fn to_string(&self) -> String {
       match self {
          Term::Var(v,_vt) => { v.clone() },
-         Term::Abs(ps,_pt) => { "λ".to_string() },
+         Term::Abs(_ps,_pt) => { "λ".to_string() },
          Term::App(l,r,_t) => { format!("({} {})", l.to_string(), r.to_string()) },
       }
    }
@@ -56,6 +57,10 @@ impl Term {
 
          //sections
          if let Term::Var(ref dir,_) = **dir {
+         if dir==".ascii" {
+            return format!(".ascii {}\n", r.as_assembly());
+         }}
+         if let Term::Var(ref dir,_) = **dir {
          if dir.starts_with(".") {
             return format!("{}\n{}", dir.to_string(), r.as_assembly());
          }}
@@ -66,6 +71,11 @@ impl Term {
          }}}
 
          //labels
+         if let Term::Var(ref dir,_) = **dir {
+         if dir=="label" {
+         if let Term::Var(ref ln,_) = **r {
+            return format!("{}:\n", ln.to_string());
+         }}}
          if let Term::Var(ref dir,_) = **dir {
          if dir=="label" {
          if let Term::App(ref ln,ref inner,_) = **r {
@@ -82,13 +92,17 @@ impl Term {
 
          //instructions
          if let Term::Var(ref dir,_) = **dir {
+         if ZOP.contains(&dir.as_str()) {
+            return format!("\t{}\n{}", dir, r.as_assembly());
+         }}
+         if let Term::Var(ref dir,_) = **dir {
          if UNOP.contains(&dir.as_str()) {
             return format!("\t{} {}\n", dir, r.as_assembly());
          }}
          if let Term::Var(ref dir,_) = **dir {
          if BINOP.contains(&dir.as_str()) {
          if let Term::App(ref a1,ref a2,_) = **r {
-            return format!("\t{} {} {}\n", dir, a1.as_assembly(), a2.as_assembly());
+            return format!("\t{} {}, {}\n", dir, a1.as_assembly(), a2.as_assembly());
          }}}
          if let Term::App(ref ldir,ref inner,_) = **dir {
          if let Term::Var(ref ldir,_) = **ldir {
@@ -99,12 +113,19 @@ impl Term {
          if let Term::Var(ref ldir,_) = **ldir {
          if BINOP.contains(&ldir.as_str()) {
          if let Term::App(ref a1,ref a2,_) = **inner {
-            return format!("\t{} {} {}\n{}", ldir, a1.as_assembly(), a2.as_assembly(), r.as_assembly());
+            return format!("\t{} {}, {}\n{}", ldir, a1.as_assembly(), a2.as_assembly(), r.as_assembly());
          }}}}
-
-         panic!("unknown directive: {} {}", dir.to_string(), r.to_string());
       }
-      self.to_string()
+      if let Term::Var(v, _) = self {
+         if ZOP.contains(&v.as_str()) {
+            return format!("\t{}\n", v);
+         }
+         if v.starts_with("@") {
+            return format!("${}", &v[1..]);
+         }
+         return v.clone();
+      }
+      panic!("unknown directive: {}", self.to_string());
    }
    pub fn compile(&self, cfg: &str) {
       let assembly = self.as_assembly();
--- tests/c_as.rs
+++ tests/c_as.rs
@@ -7,19 +7,18 @@ fn hello_world() {
       (.global _start)
       (.text)
       (label _start
-         (movl @4 %eax) 
-         (movl @1 %ebx)
-         (movl @msg %ecx)
-         (movl @len %edx)
-         (int @0x80)
-         (movl @1 %eax)
-         (movl @0 %ebx)
-         (int @0x80)
+         (mov @1 %rax)
+         (mov @1 %rdi)
+         (mov @message %rsi)
+         (mov @13 %rdx)
+         (syscall)
+
+         (mov @60 %rax)
+         (xor %rdi %rdi)
+         (syscall)
       )
-      (.data)
-      (label msg
-         (.ascii "Hello, world!\n")
-         (.len offset @len @msg)
+      (label message
+         (.ascii "Hello, world\n")
       )
    );
    program.compile("a.out");
