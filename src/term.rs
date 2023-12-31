38e39dbdfb70ae753da782f9f8e330baa66881de
Andrew Johnson <andrew@subarctic.org>
2023-12-30 20:35:38 -0700

    remove assembly junk to higher layer

--- src/term.rs
+++ src/term.rs
@@ -1,10 +1,6 @@
 
 use crate::typ::Type;
 use crate::reference_solver::infer;
-use std::fs::File;
-use std::io::prelude::*;
-use std::process::Command;
-use std::io::BufReader;
 
 #[derive(Debug,PartialEq,Eq,Clone)]
 pub enum Term {
@@ -12,33 +8,6 @@ pub enum Term {
    Abs(Vec<(Term,Term)>,Type), //lambdas are potentially plural, \ <a.x> <b.y> <c.z>
    App(Box<Term>,Box<Term>,Type),
 }
-
-const LDIRECTIVES: [&str; 3] = [".ascii", ".asciz", ".zero"];
-const UDIRECTIVES: [&str; 1] = [".global"];
-const ZDIRECTIVES: [&str; 1] = [".text"];
-
-fn is_binop(s: &str) -> bool {
-   let file = File::open("opcodes/x86.yaml").expect("Could not read file opcodes/x86.yaml");
-   let buf_reader = BufReader::new(file);
-   let data: serde_yaml::Value = serde_yaml::from_reader(buf_reader)
-            .expect("Could not read file opcodes/x86.yaml");
-   !data[s]["binary"].is_null()
-}
-fn is_unop(s: &str) -> bool {
-   let file = File::open("opcodes/x86.yaml").expect("Could not read file opcodes/x86.yaml");
-   let buf_reader = BufReader::new(file);
-   let data: serde_yaml::Value = serde_yaml::from_reader(buf_reader)
-            .expect("Could not read file opcodes/x86.yaml");
-   !data[s]["unary"].is_null()
-}
-fn is_zop(s: &str) -> bool {
-   let file = File::open("opcodes/x86.yaml").expect("Could not read file opcodes/x86.yaml");
-   let buf_reader = BufReader::new(file);
-   let data: serde_yaml::Value = serde_yaml::from_reader(buf_reader)
-            .expect("Could not read file opcodes/x86.yaml");
-   !data[s]["zero"].is_null()
-}
-
 
 impl Term {
    pub fn to_string(&self) -> String {
@@ -75,119 +44,6 @@ impl Term {
          Term::Var(v,_) => Term::Var(v,tt),
          Term::Abs(a,_) => Term::Abs(a,tt),
          Term::App(f,x,_) => Term::App(f,x,tt),
-      }
-   }
-   pub fn as_assembly(&self) -> String {
-      if let Term::App(dir,r,_) = self {
-         //pointer
-         if let Term::Var(ref dir,_) = **dir {
-         if dir=="[]" {
-            return format!("({})", r.as_assembly());
-         }}
-
-         //sections
-         if let Term::Var(ref dir,_) = **dir {
-         if LDIRECTIVES.contains(&dir.as_str()) {
-            return format!("\t{} {}\n", dir, r.as_assembly());
-         }}
-         if let Term::Var(ref dir,_) = **dir {
-         if ZDIRECTIVES.contains(&dir.as_str()) {
-            return format!("{}\n{}\n", dir, r.as_assembly());
-         }}
-         if let Term::Var(ref dir,_) = **dir {
-         if UDIRECTIVES.contains(&dir.as_str()) {
-            return format!("{} {}\n", dir, r.as_assembly());
-         }}
-
-         //labels
-         if let Term::Var(ref dir,_) = **dir {
-         if dir=="label" {
-         if let Term::Var(ref ln,_) = **r {
-            return format!("{}:\n", ln.to_string());
-         }}}
-         if let Term::Var(ref dir,_) = **dir {
-         if dir=="label" {
-         if let Term::App(ref ln,ref inner,_) = **r {
-         if let Term::Var(ref ln,_) = **ln {
-            return format!("{}:\n{}", ln.to_string(), inner.as_assembly());
-         }}}}
-         if let Term::App(ref ldir,ref label,_) = **dir {
-         if let Term::Var(ref ldir,_) = **ldir {
-         if ldir=="label" {
-         if let Term::App(ref ln,ref inner,_) = **label {
-         if let Term::Var(ref ln,_) = **ln {
-            return format!("{}:\n{}\n{}", ln.to_string(), inner.as_assembly(), r.as_assembly());
-         }}}}}
-
-         //instructions
-         if let Term::Var(ref dir,_) = **dir {
-         if is_zop(&dir.as_str()) {
-            return format!("\t{}\n{}", dir, r.as_assembly());
-         }}
-         if let Term::Var(ref dir,_) = **dir {
-         if is_unop(&dir.as_str()) {
-            return format!("\t{} {}\n", dir, r.as_assembly());
-         }}
-         if let Term::Var(ref dir,_) = **dir {
-         if is_binop(&dir.as_str()) {
-         if let Term::App(ref a1,ref a2,_) = **r {
-            return format!("\t{} {}, {}\n", dir, a1.as_assembly(), a2.as_assembly());
-         }}}
-         if let Term::App(ref ldir,ref inner,_) = **dir {
-         if let Term::Var(ref ldir,_) = **ldir {
-         if is_unop(&ldir.as_str()) {
-            return format!("\t{} {}\n{}", ldir, inner.as_assembly(), r.as_assembly());
-         }}}
-         if let Term::App(ref ldir,ref inner,_) = **dir {
-         if let Term::Var(ref ldir,_) = **ldir {
-         if is_binop(&ldir.as_str()) {
-         if let Term::App(ref a1,ref a2,_) = **inner {
-            return format!("\t{} {}, {}\n{}", ldir, a1.as_assembly(), a2.as_assembly(), r.as_assembly());
-         }}}}
-
-         //sequences
-         return format!("{}{}", dir.as_assembly(), r.as_assembly());
-      }
-      if let Term::Var(v, _) = self {
-         if is_zop(&v.as_str()) {
-            return format!("\t{}\n", v);
-         }
-         if v.starts_with("@") {
-            return format!("${}", &v[1..]);
-         }
-         return v.clone();
-      }
-      panic!("unknown directive: {}", self.to_string());
-   }
-   pub fn compile(&self, cfg: &str) {
-      let assembly = self.as_assembly();
-      if cfg.ends_with(".s") {
-         let mut file = File::create(cfg).expect("Could not create file in Term::compile");
-         file.write_all(assembly.as_bytes()).expect("Could not write to file in Term::compile");
-      } else {
-         let tmp_o = format!("tmp.{}.{}.o",std::process::id(), std::thread::current().id().as_u64() );
-         let tmp_s = format!("tmp.{}.{}.s",std::process::id(), std::thread::current().id().as_u64() );
-         let mut file = File::create(&tmp_s).expect("Could not create file in Term::compile");
-         file.write_all(assembly.as_bytes()).expect("Could not write to file in Term::compile");
-
-         Command::new("as")
-                 .arg(&tmp_s)
-                 .arg("-o")
-                 .arg(&tmp_o)
-                 .spawn()
-                 .expect("Could not run assembler in Term::compile")
-                 .wait()
-                 .expect("Could not wait for assembler in Term::compile");
-
-         Command::new("ld")
-                 .arg("-s")
-                 .arg("-o")
-                 .arg(cfg)
-                 .arg(&tmp_o)
-                 .spawn()
-                 .expect("Could not run linker in Term::compile")
-                 .wait()
-                 .expect("Could not wait for linker in Term::compile");
       }
    }
 }
